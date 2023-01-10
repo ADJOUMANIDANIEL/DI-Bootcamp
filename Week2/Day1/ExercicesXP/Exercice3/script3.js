8c118ec85afa2ce56a35d06d0c14b1e2bcf1406a
Adjoumani <danielkouadio182@gmail.com>
2023-01-10 17:32:33 +0000

    Week2 - Day1 ExerciceXP

--- Week2/Day1/ExercicesXP/Exercice3/script3.js
+++
@@ -1,8 +0,0 @@
-//1- Store a celsius temperature into a variable
-
-let temperatureCelcius = 32;
-console.log(temperatureCelcius + " ° C");
-
-//2- Convert it to fahrenheit and console.log
-let temperatureFahrenheit = (temperatureCelcius/5) * 9 + 32;
-console.log(temperatureFahrenheit + " ° F");
