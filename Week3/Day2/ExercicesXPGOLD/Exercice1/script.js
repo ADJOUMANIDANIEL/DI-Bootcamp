c5d6657345fd0d4c9f9eafbcb7621b260423d66c
Adjoumani <danielkouadio182@gmail.com>
2023-01-11 10:11:00 +0000

    Week3 - Day2 ExerciceXPGOLG Exercice1

---
+++ Week3/Day2/ExercicesXPGOLD/Exercice1/script.js
@@ -0,0 +1,12 @@
+let select = document.getElementById("genres");
+
+let options = document.querySelectorAll("option");
+
+let newOption = document.createElement("option");
+newOption.innerHTML = "Classic";
+newOption.setAttribute("value", "classic");
+newOption.setAttribute("selected","");
+select.appendChild(newOption);
+
+// options[1].removeAttribute("selected");
+
